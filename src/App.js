0ad622d0ba68d57910ac8996b546c3590bcff218
Ari Azevedo <azevedo.consultor@hotmail.com>
2025-02-07 15:02:31 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      // Remover qualquer SW antigo
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((reg) => reg.unregister());
+      });
+
+      // Registrar o novo Service Worker
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((reg) => {
+          console.log("Novo Service Worker registrado!", reg);
+        })
+        .catch((err) => console.log("Erro ao registrar Service Worker:", err));
+    }
+  }, []);
+
   return (
     <div style={{ textAlign: "center", padding: "2rem", fontFamily: "Arial, sans-serif" }}>
       <h1 style={{ color: "#007BFF" }}>Bem-vindo à Minha Página React!</h1>
-      <p>
-        Esta é uma página simples feita com REACT e uso PWA.
-      </p>
+      <p>Esta é uma página simples feita com REACT e uso PWA.</p>
       <button
         style={{
           padding: "10px 20px",
@@ -17,7 +32,7 @@ function App() {
           borderRadius: "5px",
           cursor: "pointer",
         }}
-        onClick={() => {alert("Olá, Amigos!Ari,Rafaella,Rafael.Natália,Nadia e meu amor Sara")}}
+        onClick={() => alert("Olá, Amigos! Ari, Rafaella, Rafael, Natália, Nadia e meu amor Sara")}
       >
         Clique aqui!
       </button>
@@ -26,4 +41,3 @@ function App() {
 }
 
 export default App;
-
