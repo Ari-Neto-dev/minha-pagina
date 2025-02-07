e78605a91f7fd4c8add5b5f652c88a0e04c9889e
Ari Azevedo <azevedo.consultor@hotmail.com>
2025-02-07 11:04:55 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           borderRadius: "5px",
           cursor: "pointer",
         }}
-        onClick={() => alert("Olá, Amigos!Ari ")}
+        onClick={() => alert("Olá, Amigos!Ari,Rafaella,Rafael.Natália,Nadia e meu amor Sara ")}
       >
         Clique aqui!
       </button>
